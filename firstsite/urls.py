a6a8e270e6e3b7a136e54b0c0bc059ed7865b289
Ubuntu <ubuntu@ip-172-31-0-161.us-west-2.compute.internal>
2017-03-15 19:17:17 +0000

    Initial 3 commit

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import hello, current_datetime 
+from firstsites.views import hello, current_datetime 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
--- firstsite/views.py
+++ firstsite/views.py
@@ -3,7 +3,7 @@ import datetime
 
  
 def hello(request): 
-    return HttpResponse("Hello  xx world") 
+    return HttpResponse("Hello  xxx world") 
 
 def current_datetime(request):
      now = datetime.datetime.now()
